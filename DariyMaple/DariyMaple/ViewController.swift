b6ab4cf0155a0a6576eb94602929adb8ac557a77
Leejeongchan1733026 <cksqls288@gmail.com>
2022-08-04 19:07:30 +0900

    UI설정

---
+++ DariyMaple/DariyMaple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DariyMaple
+//
+//  Created by 이정찬 on 2022/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
