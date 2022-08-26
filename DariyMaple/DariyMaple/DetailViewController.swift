15884cee5e55120f5a889952909ccdb92fd71a99
Leejeongchan1733026 <cksqls288@gmail.com>
2022-08-26 21:20:27 +0900

    심볼 필드 생성

--- DariyMaple/DariyMaple/DetailViewController.swift
+++ DariyMaple/DariyMaple/DetailViewController.swift
@@ -9,7 +9,12 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var symboll: UITextField!
+    @IBOutlet weak var symbollName: UITextField!
+    @IBOutlet weak var symbollLavel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func saveButton(_ sender: UIButton) {
     }
 }
